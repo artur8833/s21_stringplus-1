43144ca2ec41d5a5138cc057de8e2ba0813bcec3
Evangeline Hubert <evangelh@hy-d3.surgut.21-school.ru>
2023-07-15 15:55:02 +0500

    edit functions

--- src/string/s21_strlen.c
+++ src/string/s21_strlen.c
@@ -2,6 +2,6 @@
 // Вычисляет длину строки str, не включая завершающий нулевой символ.
 s21_size_t s21_strlen(const char *s) {
   int n = 0;
-  while (s[n++] != '\0')
+  while (s[n++] != '\0');
   return n;
 }
--- src/string/s21_strstr.c
+++ src/string/s21_strstr.c
@@ -4,18 +4,19 @@
 // символ), которая появляется в строке haystack.
 char *s21_strstr(const char *haystack, const char *needle) {
   void *temp = S21_NULL;
-
-  // int j = 0;
-  if (s21_strlen(haystack) < s21_strlen(needle)) {
+  s21_size_t haystack_len = s21_strlen(haystack);
+  s21_size_t needle_len = s21_strlen(needle);
+  if (haystack_len < needle_len) {
     temp = S21_NULL;
   } else {
     s21_size_t i = 0;
-    while (i < s21_strlen(haystack)){
+
+    while (i < haystack_len - needle_len + 1){
       s21_size_t j = 1;
       char * ha = (void*)haystack + i;
       char * ne = (void*)needle;
       if(*ha == *ne){
-        while(j < s21_strlen(needle)){
+        while(j < needle_len){
           if(*ha++ != *ne++) {
             i++;
             break;
@@ -23,34 +24,11 @@ char *s21_strstr(const char *haystack, const char *needle) {
           j++;
         } 
       } else i++;
-      if(j == s21_strlen(needle)) {
+      if(j == needle_len) {
         temp = (void*)haystack + i;
         break;
       }
-      // for(s21_size_t j = 0; j < s21_strlen(needle); j++){
-      //   i++;
-      //   ne++;
-      // }
     }
   }
-    // j = 0;
-    // while ((temp = s21_strchr(haystack, needle[j])) == S21_NULL) {
-    //   j++;
-    //   break;
-    // }
-    // printf("%c<-\t->%c%d\n", haystack[i], needle[j], j);
-  //   for (s21_size_t z = 0; z < s21_strlen(needle); z++) {
-  //     if (haystack[i + z] != needle[z]) {
-  //       temp = S21_NULL;
-  //       i++;
-  //       break;
-  //     } else if (z < s21_strlen(needle)) {
-  //       temp = (void *)haystack + i;
-  //     }
-  //   }
-  //   if (temp != S21_NULL)
-  //     break;
-  // }
-    
   return temp;
 }
