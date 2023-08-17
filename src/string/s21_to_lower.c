4a6743d78ae418eb698c990d73df4587d3b07a89
Artur <Artur>
2023-08-17 07:49:26 +0000

    Fix

--- src/string/s21_to_lower.c
+++ src/string/s21_to_lower.c
@@ -1,28 +1,22 @@
 #include "../s21_string.h"
 // Возвращает копию строки (str), преобразованной в нижний регистр
 void *s21_to_lower(const char *str) {
-  char *result = S21_NULL;
+  char *str_copy = malloc(s21_strlen(str) + 1);  // +1 для нулевого символа
+  void *result = S21_NULL;
 
-  if (str) {  // Проверка, что входная строка не является NULL
-    s21_size_t len = s21_strlen(str);  // Вычисление длины входной строки
-    char *our_str1 = (char *)malloc(
-        (len + 1) * sizeof(char));  // Выделение памяти для новой строки
-    if (our_str1) {  // Проверка, что память была успешно выделена
-      for (s21_size_t i = 0; i <= len;
-           i++) {  // Итерация по каждому символу входной строки
-        if (str[i] >= 'A' && str[i] <= 'Z') {  // Проверка, является ли симвоk
-                                               // символом верхнего регистра
-          our_str1[i] =
-              str[i] + 'a' - 'A';  // Преобразование символа верхнего регистра в
-                                   // символ нижнего регистра
-        } else {
-          our_str1[i] = str[i];  // Символ не является символом верхнего
-                                 // регистра, оставляем его без изменений
-        }
-      }
-      our_str1[len] = '\0';  // Добавление нулевого символа в конец новой строки
-      result = our_str1;  // Присвоение указателю result значения новой строки
+  int i;
+  for (i = 0; *str != '\0'; i++) {
+    str_copy[i] = *str;
+    str++;
+  }
+  str_copy[i] = '\0';
+
+  for (i = 0; str_copy[i] != '\0'; i++) {
+    if (str_copy[i] >= 'A' && str_copy[i] <= 'Z') {
+      str_copy[i] += 32;
     }
   }
+
+  result = (void *)str_copy;
   return result;
 }
--- src/string/s21_to_upper.c
+++ src/string/s21_to_upper.c
@@ -1,27 +1,22 @@
 #include "../s21_string.h"
 // Возвращает копию строки (str), преобразованной в верхний регистр
 void *s21_to_upper(const char *str) {
-  char *result = S21_NULL;
+  char *str_copy = malloc(s21_strlen(str) + 1);  // +1 для нулевого символа
+  void *result = S21_NULL;
 
-  if (str) {  // Проверка, что входная строка не является NULL
-    s21_size_t len = s21_strlen(str);  // Вычисление длины входной строки
-    char *our_str1 = (char *)malloc(
-        (len + 1) * sizeof(char));  // Выделение памяти для новой строки
-    if (our_str1) {  // Проверка, что память была успешно выделена
-      for (s21_size_t i = 0; i <= len;
-           i++) {  // Итерация по каждому символу входной строки
-        if (str[i] >= 'a' &&
-            str[i] <= 'z') {  // Проверка, является ли символ строчной буквой
-          our_str1[i] = str[i] - 'a' +
-                        'A';  // Преобразование строчной буквы в верхний регистр
-        } else {
-          our_str1[i] = str[i];  // Символ не является строчной буквой,
-                                 // оставляем его без изменений
-        }
-      }
-      our_str1[len] = '\0';  // Добавление нулевого символа в конец новой строки
-      result = our_str1;  // Присвоение указателю result значения новой строки
+  int i;
+  for (i = 0; *str != '\0'; i++) {
+    str_copy[i] = *str;
+    str++;
+  }
+  str_copy[i] = '\0';
+
+  for (i = 0; str_copy[i] != '\0'; i++) {
+    if (str_copy[i] >= 'a' && str_copy[i] <= 'z') {
+      str_copy[i] -= 32;
     }
   }
+
+  result = (void *)str_copy;
   return result;
 }
